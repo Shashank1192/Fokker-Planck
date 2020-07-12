eeafd121f6dab1e30dce2ed2a8ba750bf8d60643
pinakm9 <pinakm9@gmail.com>
2020-07-13 00:41:58 +0530

    dgm full network implemented

--- python/solve.py
+++ python/solve.py
@@ -3,36 +3,35 @@ import keras.layers as kl
 import keras.activations as ka
 import tensorflow as tf
 
-def dgm_layer(x, s1, sl, node_count):
-    z_x = tf.keras.layers.Dense(units = node_count, activation = None, name = 'my_dense')(x)
-    z_s = tf.keras.layers.Dense(units = node_count, activation = None, use_bias = False)(sl)
-    z = tf.keras.activations.tanh(tf.keras.layers.Add()([z_x, z_s]))
-    g_x = tf.keras.layers.Dense(units = node_count, activation = None)(x)
-    g_s = tf.keras.layers.Dense(units = node_count, activation = None, use_bias = False)(s1)
-    g = tf.keras.activations.tanh(tf.keras.layers.Add()([g_x, g_s]))
-    r_x = tf.keras.layers.Dense(units = node_count, activation = None)(x)
-    r_s = tf.keras.layers.Dense(units = node_count, activation = None, use_bias = False)(sl)
-    r = tf.keras.activations.tanh(tf.keras.layers.Add()([r_x, r_s]))
-    h_x = tf.keras.layers.Dense(units = node_count, activation = None)(x)
-    h_sr = tf.keras.layers.Dense(units = node_count, activation = None, use_bias = False)(tf.keras.layers.Multiply()([sl, r]))
-    h = tf.keras.activations.tanh(tf.keras.layers.Add()([h_x, h_sr]))
-    _g = tf.keras.layers.Subtract()([tf.ones_like(g), g])
-    _gh = tf.keras.layers.Multiply()([_g, h])
-    zs = tf.keras.layers.Multiply()([z, sl])
-    return tf.keras.layers.Add()([_gh, zs])
+def dgm_layer(l, x, s_1, s_l, node_count):
+    z_x = tf.keras.layers.Dense(units = node_count, activation = None, name = 'z_x_' + str(l))(x)
+    z_s = tf.keras.layers.Dense(units = node_count, activation = None, use_bias = False, name = 'z_s_' + str(l))(s_l)
+    z = tf.keras.activations.tanh(tf.keras.layers.Add(name = 'z_' + str(l))([z_x, z_s]))
+    g_x = tf.keras.layers.Dense(units = node_count, activation = None, name = 'g_x_' + str(l))(x)
+    g_s = tf.keras.layers.Dense(units = node_count, activation = None, use_bias = False, name = 'g_s_' + str(l))(s_1)
+    g = tf.keras.activations.tanh(tf.keras.layers.Add(name = 'g_' + str(l))([g_x, g_s]))
+    r_x = tf.keras.layers.Dense(units = node_count, activation = None, name = 'r_x_' + str(l))(x)
+    r_s = tf.keras.layers.Dense(units = node_count, activation = None, use_bias = False, name = 'r_s_' + str(l))(s_l)
+    r = tf.keras.activations.tanh(tf.keras.layers.Add(name = 'r_' + str(l))([r_x, r_s]))
+    h_x = tf.keras.layers.Dense(units = node_count, activation = None, name = 'h_x_' + str(l))(x)
+    h_sr = tf.keras.layers.Dense(units = node_count, activation = None, use_bias = False)(tf.keras.layers.Multiply(name = 'h_sr_' + str(l))([s_l, r]))
+    h = tf.keras.activations.tanh(tf.keras.layers.Add(name = 'h' + str(l))([h_x, h_sr]))
+    one__g = tf.keras.layers.Subtract(name = 'one__g_' + str(l))([tf.ones_like(g), g])
+    one__gh = tf.keras.layers.Multiply(name = 'one__gh_' + str(l))([one__g, h])
+    zs = tf.keras.layers.Multiply(name = 'zs' + str(l))([z, s_l])
+    return tf.keras.layers.Add(name = 's_' + str(l+1))([one__gh, zs])
+
+def dgm_model(space_dim, node_count, hidden_layer_count, name = "Fokker-Planck solver"):
+    x = tf.keras.Input(shape = (space_dim + 1, 1), name = 'x')
+    s_1 = tf.keras.layers.Dense(units = node_count, activation = 'tanh', name = 's_1')(x)
+    s_l = s_1
+    for l in range(1, hidden_layer_count):
+        s_l = dgm_layer(l = l, x = x, s_1 = s_1, s_l = s_l, node_count = node_count)
+    f_x = tf.keras.layers.Dense(units = 1, activation = None, name = 'f_x')(s_l)
+    model = tf.keras.Model(inputs = x, outputs = f_x, name = name)
+    tf.keras.utils.plot_model(model, "../images/{}.png".format(model.name), show_shapes=True)
+    return model
 
 
-node_count, space_dim = 7, 1
-x = tf.Variable([[1, 2], [3, 4]])
-s1 = tf.keras.layers.Dense(units = node_count, activation="relu")(x)
-tf.print(s1)
-z = dgm_layer(x, s1, s1, node_count = node_count)
-print(z)
-
-x = tf.keras.Input(shape = (space_dim + 1, 1))
-print("shape o f x = {}".format(x.shape))
-s1 = tf.keras.layers.Dense(units = node_count, activation="relu")(x)
-s2 = dgm_layer(x, s1, s1, node_count)
-model = tf.keras.Model(inputs = x, outputs = s2, name="Fokker-Planck solver")
+model = dgm_model(space_dim = 2, node_count = 50, hidden_layer_count = 4)
 model.summary()
-tf.keras.utils.plot_model(model, "../images/{}.png".format(model.name), show_shapes=True)
