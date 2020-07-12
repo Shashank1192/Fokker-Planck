cfa51e9352576db92433530e75e669af562d91db
pinakm9 <pinakm9@gmail.com>
2020-07-12 22:47:28 +0530

    dgm_layer in model, num of trainable parameters to be fixed

--- python/solve.py
+++ python/solve.py
@@ -3,8 +3,8 @@ import keras.layers as kl
 import keras.activations as ka
 import tensorflow as tf
 
-def dgm_layer(x, s1, sl, node_count, space_dim):
-    z_x = tf.keras.layers.Dense(units = node_count, activation = None)(x)
+def dgm_layer(x, s1, sl, node_count):
+    z_x = tf.keras.layers.Dense(units = node_count, activation = None, name = 'my_dense')(x)
     z_s = tf.keras.layers.Dense(units = node_count, activation = None, use_bias = False)(sl)
     z = tf.keras.activations.tanh(tf.keras.layers.Add()([z_x, z_s]))
     g_x = tf.keras.layers.Dense(units = node_count, activation = None)(x)
@@ -16,16 +16,23 @@ def dgm_layer(x, s1, sl, node_count, space_dim):
     h_x = tf.keras.layers.Dense(units = node_count, activation = None)(x)
     h_sr = tf.keras.layers.Dense(units = node_count, activation = None, use_bias = False)(tf.keras.layers.Multiply()([sl, r]))
     h = tf.keras.activations.tanh(tf.keras.layers.Add()([h_x, h_sr]))
-    _g = tf.keras.layers.Subtract()([tf.ones((x.shape[0], node_count)), g])
+    _g = tf.keras.layers.Subtract()([tf.ones_like(g), g])
     _gh = tf.keras.layers.Multiply()([_g, h])
     zs = tf.keras.layers.Multiply()([z, sl])
     return tf.keras.layers.Add()([_gh, zs])
 
 
-node_count, space_dim = 5, 1
+node_count, space_dim = 7, 1
 x = tf.Variable([[1, 2], [3, 4]])
 s1 = tf.keras.layers.Dense(units = node_count, activation="relu")(x)
 tf.print(s1)
-z = dgm_layer(x, s1, s1, node_count = node_count, space_dim = space_dim)
+z = dgm_layer(x, s1, s1, node_count = node_count)
 print(z)
-print()
+
+x = tf.keras.Input(shape = (space_dim + 1, 1))
+print("shape o f x = {}".format(x.shape))
+s1 = tf.keras.layers.Dense(units = node_count, activation="relu")(x)
+s2 = dgm_layer(x, s1, s1, node_count)
+model = tf.keras.Model(inputs = x, outputs = s2, name="Fokker-Planck solver")
+model.summary()
+tf.keras.utils.plot_model(model, "../images/{}.png".format(model.name), show_shapes=True)
