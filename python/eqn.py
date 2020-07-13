322d68c7e80af7be8856e10195bbdd2623c8253b
pinakm9 <pinakm9@gmail.com>
2020-07-13 23:07:04 +0530

    eqn.py with domain_sampler

---
+++ python/eqn.py
@@ -0,0 +1,49 @@
+# This is a Python module for implementing some useful classes partial differential equations
+import numpy as np
+import utility as ut
+
+class QuasiLinearPDE0(object):
+    """
+    Implements Fokker-Planck type quasi-linear parabolic PDEs
+    u_t + Lu = 0, (t, x) in box domain
+    u(0, x) = initial condition
+    u(t, x) = g(t, x) boundary condition at the boundary of the space domain
+    """
+    def __init__(self, diff_op, init_cond, bdry_cond, space_domain, time_domain):
+        """
+        Description: Constructor for QuasiLinearPDE0
+        Args:   diff_op: differential operator L
+                init_con: initial condition u(0, x)
+                bdry_cond: boundary condition u(t, x) at the boundary of the space domain
+                space_domain: box doamin for space in form of a dx2 matrix, d = space dimension
+                time_domain: domain of time as a list/tuple/np.array [a, b]
+        """
+        self.diff_op = diff_op
+        self.init_cond = init_cond
+        self.bdry_cond = bdry_cond
+        self.space_domain = np.array(space_domain)
+        self.time_domain = time_domain
+        self.space_dim = self.space_domain.shape[0]
+
+    @ut.timer
+    def domain_sampler(self, num_samples):
+        """
+        Description: sampling function for space-time domain
+        Args: number of samples to generate
+        Returns: an np.array where each row each is a singular sample from the space=-time domain with the last coordinate being time
+        """
+        self.samples = np.zeros((num_samples, self.space_dim + 1))
+        for j in range(self.space_dim):
+            a, b = self.space_domain[j]
+            self.samples[:, j] = (b - a)*np.random.random(num_samples) + a
+        a, b = self.time_domain
+        self.samples[:, self.space_dim] = (b - a)*np.random.random(num_samples) + a
+        return self.samples
+
+
+"""
+fp = QuasiLinearPDE0(None, None, None, [[1, 2], [5, 9], [10, 67], [-3, 7]], [4, 5])
+fp.domain_sampler(10000)
+print(fp.samples)
+print(fp.samples[:, 0], len(fp.samples[:, 4]))
+"""
--- python/solve.py
+++ python/solve.py
@@ -1,37 +1,46 @@
-import keras as ks
-import keras.layers as kl
-import keras.activations as ka
 import tensorflow as tf
+import eqn
 
-def dgm_layer(l, x, s_1, s_l, node_count):
-    z_x = tf.keras.layers.Dense(units = node_count, activation = None, name = 'z_x_' + str(l))(x)
-    z_s = tf.keras.layers.Dense(units = node_count, activation = None, use_bias = False, name = 'z_s_' + str(l))(s_l)
+# creates layer S_(l+1) from S_l, x and S_1 according to the DGM paper  ---->  https://arxiv.org/abs/1708.07469
+def dgm_layer(l, x, s_1, s_l, num_nodes):
+    z_x = tf.keras.layers.Dense(units = num_nodes, activation = None, name = 'z_x_' + str(l))(x)
+    z_s = tf.keras.layers.Dense(units = num_nodes, activation = None, use_bias = False, name = 'z_s_' + str(l))(s_l)
     z = tf.keras.activations.tanh(tf.keras.layers.Add(name = 'z_' + str(l))([z_x, z_s]))
-    g_x = tf.keras.layers.Dense(units = node_count, activation = None, name = 'g_x_' + str(l))(x)
-    g_s = tf.keras.layers.Dense(units = node_count, activation = None, use_bias = False, name = 'g_s_' + str(l))(s_1)
+    g_x = tf.keras.layers.Dense(units = num_nodes, activation = None, name = 'g_x_' + str(l))(x)
+    g_s = tf.keras.layers.Dense(units = num_nodes, activation = None, use_bias = False, name = 'g_s_' + str(l))(s_1)
     g = tf.keras.activations.tanh(tf.keras.layers.Add(name = 'g_' + str(l))([g_x, g_s]))
-    r_x = tf.keras.layers.Dense(units = node_count, activation = None, name = 'r_x_' + str(l))(x)
-    r_s = tf.keras.layers.Dense(units = node_count, activation = None, use_bias = False, name = 'r_s_' + str(l))(s_l)
+    r_x = tf.keras.layers.Dense(units = num_nodes, activation = None, name = 'r_x_' + str(l))(x)
+    r_s = tf.keras.layers.Dense(units = num_nodes, activation = None, use_bias = False, name = 'r_s_' + str(l))(s_l)
     r = tf.keras.activations.tanh(tf.keras.layers.Add(name = 'r_' + str(l))([r_x, r_s]))
-    h_x = tf.keras.layers.Dense(units = node_count, activation = None, name = 'h_x_' + str(l))(x)
-    h_sr = tf.keras.layers.Dense(units = node_count, activation = None, use_bias = False)(tf.keras.layers.Multiply(name = 'h_sr_' + str(l))([s_l, r]))
+    h_x = tf.keras.layers.Dense(units = num_nodes, activation = None, name = 'h_x_' + str(l))(x)
+    h_sr = tf.keras.layers.Dense(units = num_nodes, activation = None, use_bias = False)(tf.keras.layers.Multiply(name = 'h_sr_' + str(l))([s_l, r]))
     h = tf.keras.activations.tanh(tf.keras.layers.Add(name = 'h' + str(l))([h_x, h_sr]))
     one__g = tf.keras.layers.Subtract(name = 'one__g_' + str(l))([tf.ones_like(g), g])
     one__gh = tf.keras.layers.Multiply(name = 'one__gh_' + str(l))([one__g, h])
     zs = tf.keras.layers.Multiply(name = 'zs' + str(l))([z, s_l])
     return tf.keras.layers.Add(name = 's_' + str(l+1))([one__gh, zs])
 
-def dgm_model(space_dim, node_count, hidden_layer_count, name = "Fokker-Planck solver"):
+# creates the full DGM architechture as a tensorflow model
+def dgm_model(space_dim, num_nodes, num_hidden_layers, name = "Fokker-Planck solver"):
     x = tf.keras.Input(shape = (space_dim + 1, 1), name = 'x')
-    s_1 = tf.keras.layers.Dense(units = node_count, activation = 'tanh', name = 's_1')(x)
+    s_1 = tf.keras.layers.Dense(units = num_nodes, activation = 'tanh', name = 's_1')(x)
     s_l = s_1
-    for l in range(1, hidden_layer_count):
-        s_l = dgm_layer(l = l, x = x, s_1 = s_1, s_l = s_l, node_count = node_count)
+    for l in range(1, num_hidden_layers):
+        s_l = dgm_layer(l = l, x = x, s_1 = s_1, s_l = s_l, num_nodes = num_nodes)
     f_x = tf.keras.layers.Dense(units = 1, activation = None, name = 'f_x')(s_l)
     model = tf.keras.Model(inputs = x, outputs = f_x, name = name)
     tf.keras.utils.plot_model(model, "../images/{}.png".format(model.name), show_shapes=True)
     return model
 
-
-model = dgm_model(space_dim = 2, node_count = 50, hidden_layer_count = 4)
-model.summary()
+class DGMSolver(object):
+    """
+    Implements a Python object that solves quasi-linear parabolic PDEs using DGM architechture
+    """
+    def __init__(self, eqn, space_dim, num_nodes, num_hidden_layers, name = "DGMSolver"):
+        self.eqn = eqn
+        self.space_dim = space_dim
+        self.num_nodes = num_nodes
+        self.num_hidden_layers = num_hidden_layers
+        self.name = name
+#model = dgm_model(space_dim = 2, num_nodes = 50, num_hidden_layers = 4)
+#model.summary()
