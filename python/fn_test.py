e3b6406c9faa03a8df9de5a5019d04577c7692ab
pinakm9 <pinakm9@gmail.com>
2020-07-29 12:28:06 +0530

    Update fn_test.py

--- python/fn_test.py
+++ python/fn_test.py
@@ -4,7 +4,7 @@ import der_test as dt
 import utility as ut
 
 def func(x):
-    return x[0]**2 + x[1]*x[0] - tf.math.sqrt(x[2])
+    return x[:, 0]**2 + x[:, 1]*x[:, 0] - tf.math.sqrt(x[:, 2])
 
 def func_out(input_shape):
     return (input_shape[0], 1, )
@@ -15,8 +15,11 @@ model_ = tf.keras.Model(inputs = x, outputs = func_layer, name = 'func_model')
 @ut.tester
 @tf.function
 def model__hess(x):
-    return tf.reshape(tf.hessians(model_(x), x), [3,3])
-a = tf.reshape(tf.constant(np.array([1,2,3]), dtype=tf.float32), (3, ))
+    return tf.reshape(tf.hessians(model_(x), x), (2,2,3,3))
+a = tf.constant([[1,2,3], [4, 5, 6]], dtype=tf.float32)
+print(tf.shape(a))
+print( a[:, 0]**2 + a[:, 1]*a[:, 0] )
 dt.comp_grad(model_, a)
 model__hess(a)
 print(model_(a))
+print(tf.shape(a))
